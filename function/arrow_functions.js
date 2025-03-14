07f9c71a9ffde7440b8952c28cb4d71dabe986b2
NagendraT12 <nagendrat121212@gmail.com>
2025-03-14 15:23:40 +0530

    arrow function in js

---
+++ function/arrow_functions.js
@@ -0,0 +1,15 @@
+const mul = (a,b) => a*b;
+
+const result = mul(100,200);
+console.log(result);
+
+
+//Multi-line Arrow Function 
+
+const add = (a,b) => {
+    console.log('hii this is ur addition of 2 number ');
+    return a+b;
+}
+
+const r = add(100,100);
+console.log(r);
