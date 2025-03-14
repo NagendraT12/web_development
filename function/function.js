a2f173a28086cc22f2c88cbec6add279e8c8ab3c
NagendraT12 <nagendrat121212@gmail.com>
2025-03-14 12:17:23 +0530

    functions

---
+++ function/function.js
@@ -0,0 +1,14 @@
+// using return 
+function sum(num1,num2)//valus are passing hera
+{
+    //block code 
+    return num1+num2;
+
+}
+
+const r =sum (100,200);//value are passing here
+console.log(r);
+
+
+
+
