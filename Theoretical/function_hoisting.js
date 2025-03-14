2643ebb0b786c1cc00bea73cbdf5144b24b25a92
NagendraT12 <nagendrat121212@gmail.com>
2025-03-14 13:11:23 +0530

    hositing

---
+++ Theoretical/function_hoisting.js
@@ -0,0 +1,15 @@
+greetings();
+
+// function declaration
+function greetings(){
+   console.log("hello"); 
+}
+
+
+// fuction expression 
+greet();
+
+var greet =  () =>{
+    console.log("hiiiiiiiiiiiii");
+} 
+
